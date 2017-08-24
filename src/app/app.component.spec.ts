3c5e8b5ab1fb0fbab111a689c10107f0e56fb1bd
Tyler Roth <troth@CSMBP0004.local>
2017-08-24 11:01:53 -0400

    add one more test for input form field

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent
       ],
@@ -20,13 +22,21 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Callibrity');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Callibrity!');
+  }));
+
+  it('should put name in a p tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    app.name = 'foobar';
+    fixture.detectChanges();
+    expect(app.name).toEqual('foobar');
   }));
 });
