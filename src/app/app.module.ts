9bd1e2703b44350ce5ec345a5c6846fba4bf96eb
Tyler Roth <troth@CSMBP0004.local>
2017-08-25 08:11:36 -0400

    add ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -12,6 +13,7 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     FormsModule
   ],
   providers: [],
