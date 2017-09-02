9ca808579d32af8a9054552a5c7b015bab802d36
E.J. Schroeder <eschroeder@callibrity.com>
2017-09-02 13:35:32 -0400

    Move hero to within home folder. Add CSS to make video play as background to jumbotron. Clean up components and change navbar classes to use Bootstrap 4 beta classes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 import { HomeComponent } from './home/home.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeroComponent } from './home/hero/hero.component';
 
 const appRoutes: Routes = [ {path: '/', component: AppComponent} ]
 
@@ -18,7 +17,6 @@ const appRoutes: Routes = [ {path: '/', component: AppComponent} ]
     AppComponent,
     NavigationComponent,
     FooterComponent,
-    BodyComponent,
     HomeComponent,
     HeroComponent,
   ],
